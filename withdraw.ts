7217586ec0f9d6247d04ae10942e84f283b2fdd6
Jonny <jonny@linkpool.io>
2020-09-30 14:52:12 +0100

    Support for a withdraw threshold with check for duplicating feeds

--- withdraw.ts
+++ withdraw.ts
@@ -11,9 +11,12 @@ const walletAddress = process.env.WALLET_ADDRESS || "0x501698a6f6f762c79e4d28e38
 const rpcUrl = process.env.RPC_URL || "http://localhost:8545"
 const privateKey = process.env.PRIVATE_KEY || ""
 const withdrawTo = process.env.WITHDRAW_TO || ""
+const withdrawThreshold = process.env.WITHDRAW_THRESHOLD || "0.0"
 
 const gasPrice = process.env.GAS_PRICE || 20000000000 // 20 Gwei
 const txOptions = {gasPrice: gasPrice}
+
+let matchedFeeds = new Map()
 
 async function scanFeeds(walletAddress: string, batchSize = 20): Promise<Feed[]> {
     const page = await market.getFeeds(1, 0)
@@ -47,9 +50,23 @@ function newWallet(privateKey: string, rpcUrl: string): ethers.Wallet {
 }
 
 async function withdrawFromFeed(feed: Feed, wallet: ethers.Wallet) {
+    if (!matchedFeeds[feed.contractAddress.toString()]) {
+        matchedFeeds[feed.contractAddress.toString()] = true
+    } else {
+        log.info("Trying to withdraw on a feed already been withdrawn from, skipping")
+        return
+    }
+
     const feedContract = new ethers.Contract(feed.contractAddress.toString(), fluxAggregatorAbi, wallet)
     const withdrawableAmount = await feedContract.withdrawablePayment(walletAddress)
-    log.info("Withdrawing amount " + ethers.utils.formatEther(withdrawableAmount) + " LINK from feed " + feed.contractAddress + ", to " + withdrawTo)
+    const parsedWithdrawThreshold = ethers.utils.parseEther(withdrawThreshold)
+
+    if (withdrawableAmount.lt(parsedWithdrawThreshold)) {
+        log.info("Withdrawable amount is not greater than threshold of " + parsedWithdrawThreshold + ", skipping")
+        return
+    }
+
+    log.info("Withdrawing amount " + withdrawableAmount + " LINK from feed " + feed.contractAddress + ", to " + withdrawTo)
     await feedContract.withdrawPayment(walletAddress, withdrawTo, withdrawableAmount, txOptions)
 }
 
